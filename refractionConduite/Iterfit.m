073355b51dbf2e0eb40b3688442dd1973ab4717b
ROUILLE VALENTIN <vrouille@lea.univ-poitiers.fr>
2016-05-13 16:29:38 +0200

    modification du stockage des matrices pour ne pas prendre en compte les valeurs nulles NaN ou supérieures au rayon du cercle 2

--- refractionConduite/Iterfit.m
+++ refractionConduite/Iterfit.m
@@ -1,16 +1,16 @@
 iterations=1;
 b=0;
 while(b == 0) %tant que la valeur entrée n'est pas conforme
-iterations = input("Entrer le nombre d'iterations: 50 75 100 150 200 300 500\t-> "); 
+iterations = input("Entrer le nombre d'iterations: 10 20 50 75 100 150 200 300 500\t-> "); 
 	switch (iterations)
-	case {50, 75, 100, 150, 200, 300, 500}
+	case {10,20,50, 75, 100, 150, 200, 300, 500}
 		disp("ok");
-    %flag pour éviter de  se retrouver avec des matrices à dimensionss différentes
-		[flag, h, hrk1, hrk2]=Refraction_conduite_1(iterations); 
-		for cpt=1:7
-			m_p=fitRefracted(2*cpt,hrk1,hrk2,h, iterations,10,cpt,flag);
+		[ h, hrk1, hrk2, R2]=Refraction_conduite_1(iterations); 
+		for cpt=1:2							%ordre polynome,iterations,échantillons,cpt		   
+			m_p=fitRefracted(h,hrk1, hrk2, R2,2*cpt,iterations,10,cpt);
 		endfor
 		b=1;
-	otherwise %do nothing
+	otherwise 
+		disp("Inserer un nombre parmi les propositions ci dessous");%do nothing
 	endswitch
 endwhile
--- refractionConduite/fitRefracted.m
+++ refractionConduite/fitRefracted.m
@@ -1,56 +1,68 @@
-function m_p = fitRefracted(polynomialOrder, hrk1, hrk2, h, iterations, echantillons,cpt)%polynomialOrder, nCase, hrk1, hrk2, h, iterations, echantillons
-   	nbCol=[columns(hrk1) columns(hrk2)]% avant la mise en fome des matrices
+%polynomialOrder, nCase, hrk1, hrk2, h, iterations, echantillons
+function m_p = fitRefracted(h,hrk1, hrk2, R2, polynomialOrder, iterations, echantillons, cpt)%polynomialOrder, nCase, hrk1, hrk2, h, iterations, echantillons
 	
-	hrk1= hrk1(find(hrk1<=R2));
-	hrk2= hrk1(find(hrk2<=R2));
+	
+	
+	hrk1= hrk1(find(hrk1<=R2));	
+	hrk1= hrk1(find(hrk1 != 0)); 
+	hrk1= hrk1(find(hrk1 != NaN));
+	
+	hrk2= hrk2(find(hrk2<=R2));
+ 	hrk2= hrk2(find(hrk2 != 0));
+	hrk2= hrk2(find(hrk2 != NaN));
 	
 	test=[columns(hrk2) < columns(hrk1) columns(hrk2) > columns(hrk1)];
 	
-	if(test(1)) %+ de colonnes cas1
+	nbCol=[columns(hrk1) columns(hrk2)]
+	pause
+	if(test(1) == 1) %+ de colonnes cas1
 		hrk2=[hrk2 zeros(1,columns(hrk1)-columns(hrk2))];
-	else if (test(2))%+ de colonnes cas1
-		hrk1=[hrk1 zeros(1,columns(hrk1)-columns(hrk2))];
-	else %autant de colonnes cas1 cas2
+	elseif (test(2) == 1)%+ de colonnes cas2
+		hrk1=[hrk1 zeros(1,columns(hrk2)-columns(hrk1))];
+	else
+		%hrk1 et hrk2 on la meme taille
 	endif
-    
-	a=hrk1(1,:)-h;
-    b=hrk2(1,:)-h;
+	
+	a=hrk1(1,:)-h
+	pause
+    b=hrk2(1,:)-h
+	pause
     
 	m_cases=[a ; b];
-    m_legend=["-sg;ecart1;"; "-sg;ecart2;"; "ob;fonction;"; "-r;polynome;"];
+	string4=["-r;" "polynomeordre_{" num2str(2*cpt) "};"];
+    m_legend=["-sg;ecart1;"; "-sg;ecart2;"; "ob;fonction;" ; string4];
         
 for k=1:2
     figure(2*cpt+k) %détermination de la cohérence du polynôme avec la courbe originelle
     m_fig=plot(m_cases(k,1:nbCol(k)), m_legend(k,:));
     hold on;
-    m_x = get(m_fig, "xdata");	%abscisses
+    
+	m_x = get(m_fig, "xdata");	%abscisses
     m_y = get(m_fig, "ydata");	%ordonnees
     m_n = polynomialOrder; %ordre du polnome
     m_p = polyfit(m_x, m_y, m_n); %interpolation polynomiale
      %evaluation du polynome
-    xfit = linspace(min(m_x),max(m_x),echantillons);
+    
+	xfit = linspace(min(m_x),max(m_x),echantillons);
     yfit = polyval(m_p,xfit);
     plot(m_x,m_y,m_legend(3,:),xfit,yfit,m_legend(4,:)); %affichage de la fonction et du polynôme
     legend show Location NorthEastOutside
     saveas(2+k, ['i' num2str(iterations) '/hrk_',num2str(k),'_fitting_ordre_',num2str(polynomialOrder),'.png']);
 endfor
 	 
-	 %for test only
-	 % disp("flag :")
-	 % flag
-	 % pause;
-	 % figure(2*cpt+1)
-	 % a=hrk1(1,1:columns(hrk1))-h;
-	 % m_legend=["-sg;ecart1;"; "-sg;ecart2;"; "ob;fonction;"; "-r;polynome;"];
-	 % m_fig=plot(a, m_legend(1,:));
-     % hold on;
-     % m_x = get(m_fig, "xdata");	%abscisses
-     % m_y = get(m_fig, "ydata");	%ordonnees
-     % m_n = polynomialOrder; %ordre du polynome
-     % m_p = polyfit(m_x, m_y, m_n); %interpolation polynomiale
-	 % xfit = linspace(min(m_x),max(m_x),echantillons);
-	 % yfit = polyval(m_p,xfit);
-	 % plot(m_x,m_y,m_legend(3,:),xfit,yfit,m_legend(4,:)); 
-	 % legend show Location NorthEastOutside
-	 
-	 endfunction
+	 % %for test only
+	 % % pause;
+	 % % figure(2*cpt+1)
+	 % % a=hrk1(1,1:columns(hrk1))-h;
+	 % % m_legend=["-sg;ecart1;"; "-sg;ecart2;"; "ob;fonction;"; "-r;polynome;"];
+	 % % m_fig=plot(a, m_legend(1,:));
+     % % hold on;
+     % % m_x = get(m_fig, "xdata");	%abscisses
+     % % m_y = get(m_fig, "ydata");	%ordonnees
+     % % m_n = polynomialOrder; %ordre du polynome
+     % % m_p = polyfit(m_x, m_y, m_n); %interpolation polynomiale
+	 % % xfit = linspace(min(m_x),max(m_x),echantillons);
+	 % % yfit = polyval(m_p,xfit);
+	 % % plot(m_x,m_y,m_legend(3,:),xfit,yfit,m_legend(4,:)); 
+	 % % legend show Location NorthEastOutside
+endfunction
