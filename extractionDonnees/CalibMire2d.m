3d36982ce225a6961127e8fdc024d2c0c53b3903
ROUILLE VALENTIN <vrouille@lea.univ-poitiers.fr>
2016-05-19 12:28:42 +0200

    assemblage de plusieurs scrips pour calibrer une caméra à directement à partir de points u v relevé sur une image = 2d

---
+++ extractionDonnees/CalibMire2d.m
@@ -0,0 +1,111 @@
+function [M] = CalibMire2d(X,Y, uv)
+
+%setup coordonnées
+ spaceOnEdge=1.6774647887324;
+ spacebetweenEdgeTwoDots=4.193661971831;
+ diameterEachDot=3.3544995774648;
+ 
+ X(1)=spaceOnEdge+diameterEachDot/2;
+ Y(1)=X(1);
+ 
+ for(j=2:15)
+	X(j)=X(j-1)+diameterEachDot/2+spacebetweenEdgeTwoDots+diameterEachDot/2;
+	Y(j)=X(j);
+ endfor 
+ 
+	X=[X X X X X X X X X X X X X X X];
+	Y=[X' X' X' X' X' X' X' X' X' X' X' X' X' X' X'];
+
+%-----------------inverse matrice 2d --------------------
+
+camera1 = uv;
+
+
+%paramètres
+%Z=zeros(rows(Y),1);
+u=camera1(:,end-1);
+v=camera1(:,end);
+
+% ---- création de la matrice A ---
+% produits des matrices u par XY puis on les exprime en négatifs
+muX= -u.*X;
+muY= -u.*Y;
+mvX= -v.*X;
+mvY= -v.*Y;
+nbRows=rows(camera1);
+
+matA = [X Y ones(nbRows,1)*[1 0 0 0] muX  muY  ];
+matA= [ matA ; ones(nbRows,1)*[0 0 0] X Y ones(nbRows,1) mvX  mvY ]; 
+
+
+% ------------ matrice B ----------- 
+ matB = [u;v];
+
+
+% ---------- Calcul matrice M d'apres AM=B soit M=B* (A à la puissance moins 1)
+ M = pinv(matA)*matB;
+%passage d'une matrice 11,1 en matrice 4,3 en ajoutant m34=1
+ M = reshape([M;1],3,3)';
+% norme des trois colonnes de la troisieme ligne de M
+ norm_r3= norm(M(3,1:3));
+%On divise M par cette norme pour obtenir la position de chaque point de M 
+ M/=norm_r3;
+
+ uv = M*[X Y ones(nbRows,1)]'; %s*uv = M(3,3)*([X Y 1])
+ uv = (uv(1:2,:)./uv([3,3],:))'; %uv=suv/s
+	% affichage  des vecteurs u et v 
+   figure(1)
+   grid on;
+   
+   subplot(211);
+   plot(u,v,'+;"u et v";', uv(:,1),uv(:,2),'s;"u\&v calcules";');
+   legend boxoff 
+   legend Location NorthOutside 
+
+   subplot(223);
+   plot(uv(:,1)-u,'o;"erreur entre les deux u";');  % pour observer l'erreur de positionnement de chaque points
+   legend boxoff 
+   legend Location NorthOutside  
+   
+   subplot(224);
+   plot(uv(:,2)-v,'o;"erreur des deux v";');  % pour observer l'erreur de positionnement de chaque points
+   legend boxoff 
+   legend Location NorthOutside 
+ drawnow
+	
+%distorsion----------------------------------------------
+
+%évaluation de la distorsion de l'inversion de matrice
+
+deltaU=(uv(:,1)-u);
+deltaV=(uv(:,2)-v);
+%u v uv u² v² u²v v²u u^3 v^3 u^3*v u²v² uv^3
+
+vars{1}=[ones(rows(u), 1) u v u.*v (u.^2) (v.^2) (u.^2).*v u.*(v.^2) (u.^3) (v.^3) (u.^3.*v) u.^2.*v.^2  (u.*v.^3)];
+vars{2}=[u v u.*v (u.^2) (v.^2) (u.^2).*v u.*(v.^2) (u.^3) (v.^3) (u.^3.*v) u.^2.*v.^2  (u.*v.^3)];
+vars{3}=[ones(rows(u), 1) u.*v (u.^2) (v.^2) (u.^2).*v u.*(v.^2) (u.^3) (v.^3) (u.^3.*v) u.^2.*v.^2  (u.*v.^3) ];
+
+string{1}="polynomeFull";
+string{2}="polynomeSans1";
+string{3}="polynomeSansu_et_v";
+
+for k=1:3
+	cdu=vars{k}\deltaU; 
+	cdv=vars{k}\deltaV;
+	uCorrige=vars{k}*cdu;
+	vCorrige=vars{k}*cdv;
+	errU{k}=std(uv(:,1)-u-uCorrige); %erreur standard
+	errV{k}=std(uv(:,2)-v-vCorrige);
+	%save ('-text',['MatrixCam' '_' num2str(posCam) '_StdError_pol' string{k} '.txt'], "errU", "errV"); 
+endfor
+disp('k\terrU\terrV')
+disp(num2str(k) num2str(errU) num2str(errV));
+
+	figure(2)
+% h1=plot(allerrU(1,:));	set(h1,"color",colormap(rand(10,3)));	hold on;
+% h2=plot(allerrU(2,:));	set(h2,"color",colormap(rand(10,3)));	hold on;
+% h3=plot(allerrU(3,:));	set(h3,"color",colormap(rand(10,3)));	hold on;
+% h4=plot(allerrU(4,:));	set(h4,"color",colormap(rand(10,3)));
+	
+
+   endfunction
